b93d83bf42a5463246f31bfacfb732a28579a6ce
Zack Hanni <zack.hannI@gmail.com>
2024-03-16 17:31:57 -0400

    created responsive search bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,36 @@
 export default function Nav() {
+  const navMenuItems = [
+    { name: "Departments", link: "/" },
+    { name: "services", link: "/" },
+    { name: "Departments", link: "/" },
+  ];
+
   return (
-    <nav className="flex flex-col fixed top-0 left-0 bg-white/50 w-full">
-      <div className="flex flex-row mx-auto space-x-4 items-center h-[88px] font-bold">
+    <nav className="flex flex-col fixed top-0 left-0 bg-purple-600 w-full text-white">
+      <div className="flex flex-row justify-center space-x-4 items-center h-[88px] font-bold w-full">
         <p>OmniMart</p>
-        <p className="bg-blue-600 p-4 rounded-full">Departments</p>
-        <p>Services</p>
-        <form>
-          <input></input>
-          <button>Search</button>
+        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          Departments
+        </p>
+        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          Services
+        </p>
+        <form className="min-w-[70%] lg:min-w-fit flex">
+          <input
+            className="rounded-full px-8 py-4 font-normal w-full"
+            placeholder="Search OmniMart"
+          ></input>
+          <button>Search icon</button>
         </form>
-        <p>My Items</p>
-        <p>
+        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          My Items
+        </p>
+        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
           Account <span className="font-normal">Sign In</span>
         </p>
-        <p>Card</p>
+        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          Card
+        </p>
       </div>
       <hr />
       <div className="flex flex-row justify-evenly items-center">
