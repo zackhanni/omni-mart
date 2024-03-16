6c32126da5ff9b7b69fb583ba14c1a0920e839f9
Zack Hanni <zack.hannI@gmail.com>
2024-03-16 17:31:23 -0400

    added image section to product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,22 +3,28 @@ import Image from "next/image";
 interface ProductCardProps {
   price: string;
   name: string;
+  image: string;
 }
 
 export default function ProductCard(props: ProductCardProps) {
   return (
-    <div className="bg-yellow-800 min-w-64 h-72">
-      {/* <Image
-        src="/profile.png"
-        width={300}
-        height={500}
-        alt="Picture of the product"
-      /> */}
+    <div className="min-w-64 h-72 bg-blue-100">
+      <div className="w-full h-[70%]">
+        <Image
+          src={props.image}
+          width={300}
+          height={500}
+          alt="Picture of the product"
+          className="w-full object-cover h-full"
+        />
+      </div>
 
-      <div>
+      <div className="">
         <p>{props.price}</p>
         <p>{props.name}</p>
-        <button className="bg-blue-300 rounded-full py-1 px-4">+ Add</button>
+        <button className="outline hover:outline-2 rounded-full py-1 px-4">
+          + Add
+        </button>
       </div>
     </div>
   );
