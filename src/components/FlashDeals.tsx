c8ece24e8bb1dcc056bd1487ceae8d5c15197737
Zack Hanni <zack.hannI@gmail.com>
2024-03-18 23:29:46 -0400

    update flashDeals section with image links

--- src/components/FlashDeals.tsx
+++ src/components/FlashDeals.tsx
@@ -6,33 +6,38 @@ export default function FlashDeals() {
     {
       name: "Item 1",
       price: "$19.95",
+      image: "/product1.jpg",
     },
     {
       name: "Item 2",
       price: "$19.95",
+      image: "/product2.jpg",
     },
     {
       name: "Item 3",
       price: "$19.95",
+      image: "/product3.jpg",
     },
     {
       name: "Item 4",
       price: "$19.95",
+      image: "/product4.webp",
     },
     {
       name: "Item 5",
       price: "$19.95",
+      image: "/product1.jpg",
     },
   ];
 
   return (
-    <section className="px-4 py-8 space-y-8">
+    <section className="px-4 pt-8 pb-4 space-y-8">
       <div className="flex justify-between">
         <div>
           <h2>Flash Deals</h2>
           <p>Up to 75% off</p>
         </div>
-        <p>view all</p>
+        <p className="underline text-sm">view all</p>
       </div>
       <div className="flex space-x-4 overflow-x-scroll">
         {products.map((product) => {
@@ -41,10 +46,12 @@ export default function FlashDeals() {
               key={product.name}
               name={product.name}
               price={product.price}
+              image={product.image}
             />
           );
         })}
       </div>
+      <hr />
     </section>
   );
 }
