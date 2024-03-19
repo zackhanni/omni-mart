4f5aef106b90dbab2570b0468d1ffc786b0e02e7
Zack Hanni <zack.hannI@gmail.com>
2024-03-19 00:53:54 -0400

    spaced out card ad components

--- src/components/CreditCardAd.tsx
+++ src/components/CreditCardAd.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CreditCardAd() {
   return (
-    <div className="flex justify-center items-center p-4 rounded-md mx-4 shadow-xl outline outline-gray-100">
+    <div className="flex justify-center items-center p-4 rounded-md mx-4 shadow-xl outline outline-gray-100 space-x-4">
       <p>
         <span>Earn 5% cash back.</span> See if you&apos;re pre-approved with no
         credit risk.
