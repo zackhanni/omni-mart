7f4999c853599df48b8fd2ffde1dfc248e8f52df
Zack Hanni <zack.hannI@gmail.com>
2024-03-19 16:59:51 -0400

    added font-awesome icons to nav elements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,15 @@
+import {
+  faHeart,
+  faRectangleList,
+  faUser,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faBellConcierge,
+  faCartShopping,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Nav() {
   const navMenuItems = [
     { name: "Departments", link: "/" },
@@ -6,41 +18,50 @@ export default function Nav() {
   ];
 
   return (
-    <nav className="flex flex-col fixed top-0 left-0 bg-purple-600 w-full text-white">
-      <div className="flex flex-row justify-center space-x-4 items-center h-[88px] font-bold w-full">
+    <nav className="flex flex-col fixed top-0 left-0  w-full z-10">
+      <div className="bg-purple-600 text-white flex flex-row justify-center space-x-4 items-center h-[88px] font-bold w-full z-30">
         <p>OmniMart</p>
         <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          <FontAwesomeIcon icon={faRectangleList} className="pr-2" />
           Departments
         </p>
         <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          <FontAwesomeIcon icon={faBellConcierge} className="pr-2" />
           Services
         </p>
-        <form className="min-w-[70%] lg:min-w-fit flex">
+        <form className="min-w-[60%] lg:min-w-fit flex">
           <input
-            className="rounded-full px-8 font-normal w-full"
+            className="rounded-full px-8 py-2 font-normal w-full"
             placeholder="Search OmniMart"
           ></input>
-          <button>Search icon</button>
+          <button className="pl-3 pr-1 hover:bg-purple-800 rounded-full text-center">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="pr-2" />
+          </button>
         </form>
         <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
+          <FontAwesomeIcon icon={faHeart} className="pr-2" />
           My Items
         </p>
-        <p className="hover:bg-purple-800 p-4 rounded-full hidden lg:block">
-          Account <span className="font-normal">Sign In</span>
-        </p>
+        <div className="hover:bg-purple-800 p-4 rounded-full hidden lg:flex items-center">
+          <FontAwesomeIcon icon={faUser} className="pr-2" />
+          <div className="flex flex-col">
+            <span className="font-normal text-sm">Sign In</span>
+            Account
+          </div>
+        </div>
         <div className="hover:bg-purple-800 p-4 rounded-full flex flex-col">
-          Cart icon<p className="text-xs font-normal">$0.00</p>
+          <FontAwesomeIcon icon={faCartShopping} />
+          <p className="text-xs font-normal">$0.00</p>
         </div>
       </div>
-      <hr />
-      <div className="flex flex-row justify-evenly items-center py-2 text-sm">
+      <div className="bg-purple-300 text-black flex flex-row justify-evenly items-center py-2 text-sm">
         <p>Philadelphia, 19148 - Milkyway Supercenter</p>
-        <p className="hidden sm:block">Deals</p>
-        <p className="hidden lg:block">Grocery</p>
-        <p className="hidden md:block">Essentials</p>
-        <p className="hidden lg:block">Fashion</p>
-        <p className="hidden md:block">Ship</p>
-        <p className="hidden lg:block">Electronics</p>
+        <p className="hidden sm:block hover:underline">Deals</p>
+        <p className="hidden lg:block hover:underline">Grocery</p>
+        <p className="hidden md:block hover:underline">Essentials</p>
+        <p className="hidden lg:block hover:underline">Fashion</p>
+        <p className="hidden md:block hover:underline">Ship</p>
+        <p className="hidden lg:block hover:underline">Electronics</p>
       </div>
     </nav>
   );
