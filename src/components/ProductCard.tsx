a78b87bb20fab2b89d5a90f0b5501c4b2b0fb4d6
Zack Hanni <zack.hannI@gmail.com>
2024-03-19 17:00:32 -0400

    added heart icon to product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faSolidHeart } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 interface ProductCardProps {
@@ -7,9 +13,22 @@ interface ProductCardProps {
 }
 
 export default function ProductCard(props: ProductCardProps) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  if (props.name.length > 45) {
+    props.name = props.name.slice(0, 45 - props.name.length) + "...";
+  }
+
   return (
-    <div className="min-w-64 h-72 bg-blue-100">
-      <div className="w-full h-[70%]">
+    <div className="min-w-48 h-72 relative overflow-y-hidden ">
+      <div className="top-2 right-2 absolute text-xl">
+        <FontAwesomeIcon
+          icon={isFavorite ? faSolidHeart : faHeart}
+          onClick={() => setIsFavorite(!isFavorite)}
+        />
+      </div>
+
+      <div className="w-full h-[50%]">
         <Image
           src={props.image}
           width={300}
@@ -19,10 +38,10 @@ export default function ProductCard(props: ProductCardProps) {
         />
       </div>
 
-      <div className="">
-        <p>{props.price}</p>
-        <p>{props.name}</p>
-        <button className="outline hover:outline-2 rounded-full py-1 px-4">
+      <div className="py-4">
+        <p className="font-bold">{props.price}</p>
+        <p className="py-2">{props.name}</p>
+        <button className="outline hover:outline-2 rounded-full py-1 px-4 ml-1">
           + Add
         </button>
       </div>
