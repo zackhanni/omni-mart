84a480d172a6cea43d1667cfcc8dae824fcdf98a
Zack Hanni <zack.hannI@gmail.com>
2024-03-20 11:19:34 -0400

    adjust spacing of footer. added max width for larger screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,43 +26,53 @@ export default function Footer() {
   };
 
   return (
-    <footer className="bg-green-200 px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-        <div>Logo here</div>
-        <div>
-          <h3 className="font-bold">Featured</h3>
-          {footerLinks.featured.map((link) => {
-            return (
-              <Link key={link.name} href={link.link}>
-                <p className="hover:underline">{link.name}</p>
-              </Link>
-            );
-          })}
-        </div>
-
-        <div>
-          <h3 className="font-bold">Contact us</h3>
-          <p>Address</p>
-          <p>{contact.address}</p>
-          <p>Phone</p>
-          <p>{contact.phone}</p>
-          <p>Email</p>
-          <p>{contact.email}</p>
-        </div>
-
-        <div>
-          <h3 className="font-bold">Instagram</h3>
-          {footerLinks.socials.map((link) => {
-            return (
-              <div key={link.name}>
-                <p>{link.name}</p>
-              </div>
-            );
-          })}
-        </div>
+    <footer>
+      <div className="flex flex-col items-center bg-purple-300 py-8 space-y-4">
+        <p className="">We&apos;d love to year what you think!</p>
+        <button className=" font-bold outline outline-1 rounded-full py-2 px-4 bg-white">
+          Give feedback
+        </button>
       </div>
-      <div className="text-center py-4">All social media icons here</div>
-      <p className="text-center text-xs">2024 OmniMart. All rights reserved.</p>
+      <div className="bg-purple-600 text-white py-4 space-y-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-10 px-4 max-w-screen-xl mx-auto">
+          <div>Logo here</div>
+          <div>
+            <h3 className="font-bold">Featured</h3>
+            {footerLinks.featured.map((link) => {
+              return (
+                <Link key={link.name} href={link.link}>
+                  <p className="hover:underline">{link.name}</p>
+                </Link>
+              );
+            })}
+          </div>
+
+          <div>
+            <h3 className="font-bold">Contact us</h3>
+            <p>Address</p>
+            <p>{contact.address}</p>
+            <p>Phone</p>
+            <p>{contact.phone}</p>
+            <p>Email</p>
+            <p>{contact.email}</p>
+          </div>
+
+          <div>
+            <h3 className="font-bold">Instagram</h3>
+            {footerLinks.socials.map((link) => {
+              return (
+                <div key={link.name}>
+                  <p>{link.name}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="text-center py-4">All social media icons here</div>
+        <p className="text-center text-xs">
+          2024 OmniMart. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
