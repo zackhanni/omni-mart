942171ed181c81f6316036cbc96e122795b318ad
Zack Hanni <zack.hannI@gmail.com>
2024-03-26 23:42:29 -0400

    update product links

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,34 +4,34 @@ export const productCategory = [
     description:
       "Shape the future of humanity on distant worlds with OmniMart's astrobiology and terraforming tools. From microbial inoculants to portable greenhouse kits, we provide the resources needed to transform alien landscapes into habitable environments.",
     price: "$19.95",
-    image: "/product1.jpg",
+    image: "/istockphoto-1176397552-1024x1024.jpg",
   },
   {
     name: "Interstellar Travel Essentials",
     description:
       "Prepare for your journey across the stars with our selection of interstellar travel essentials. From hyperdrive fuel cells to advanced navigation systems, OmniMart ensures you have the tools and resources necessary to navigate the vastness of space.",
     price: "$19.95",
-    image: "/product2.jpg",
+    image: "/pexels-mikhail-nilov-7664624.jpg",
   },
   {
     name: "Advanced Technology",
     description:
       "Stay ahead of the technological curve with OmniMart's collection of advanced gadgets. Whether you need a personal AI assistant or augmented reality glasses, our range of futuristic devices will enhance your daily life both on and off-world.",
     price: "$19.95",
-    image: "/product3.jpg",
+    image: "/pexels-kindel-media-8982664.jpg",
   },
   {
     name: "Universal Communication Devices",
     description:
       "Break down language barriers and stay connected across the stars with OmniMart's universal communication devices. Whether you're sending a message across the galaxy or communicating with extraterrestrial species, our secure and reliable devices have you covered.",
     price: "$19.95",
-    image: "/product4.webp",
+    image: "/pexels-petr-ganaj-17818760.jpg",
   },
   {
     name: "Medical Supplies & Food Cube Rations",
     description:
       "Ensure your health and well-being in the harsh conditions of space with OmniMart's advanced medical supplies. From portable diagnostic devices to emergency trauma kits, we offer everything you need to handle medical emergencies with confidence. ///Satisfy your hunger in the void of space with OmniMart's space-friendly food and nutrition options. From nutrient-packed meal replacements to hydroponic gardening kits, we provide innovative solutions to keep you nourished during your cosmic travels.",
     price: "$19.95",
-    image: "/product1.jpg",
+    image: "/pexels-anna-shvets-6291281.jpg",
   },
 ];
