befc61c57fc4f8e4a85700909c2dbeb8d106f857
Zack Hanni <zack.hannI@gmail.com>
2024-03-26 23:44:16 -0400

    adjusted responsive nav content

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -21,7 +21,7 @@ export default function Nav() {
 
   return (
     <nav className="flex flex-col fixed top-0 left-0 w-full z-10 bg-gradient-to-r from-indigo-950 to-purple-700 text-white">
-      <div className="flex flex-row justify-center space-x-4 items-center h-[88px] font-bold w-full z-30 px-4">
+      <div className="flex flex-row justify-center space-x-4 items-center h-[88px] font-bold w-full px-4">
         <div className="">
           <Logo />
         </div>
@@ -54,28 +54,31 @@ export default function Nav() {
             Account
           </div>
         </div>
-        <div className="hover:bg-purple-800 p-4 rounded-full flex flex-col">
+        <div className="flex flex-col">
           <FontAwesomeIcon icon={faCartShopping} />
           <p className="text-xs font-normal pt-1">$0.00</p>
         </div>
       </div>
-      <hr />
-      <div className="flex flex-row space-x-8 items-center px-4 py-2 text-sm">
-        <p>Philadelphia, 19148 - Milkyway Supercenter</p>
-        <p>|</p>
-        <p className="lg:hidden block hover:underline font-semibold">
+      <hr className="mt-2" />
+      <div className="flex flex-row space-x-6 items-center px-4">
+        <div className="flex">
+          <p className="hidden md:block">Philadelphia, 19148 - </p>
+          <p>Milkyway Supercenter</p>
+        </div>
+        <p className="hidden sm:block">|</p>
+        <p className="lg:hidden sm:block hidden hover:underline font-semibold">
           Departments
         </p>
-        <p className="xl:hidden block hover:underline font-semibold">
+        <p className="xl:hidden sm:block hidden hover:underline font-semibold">
           Services
         </p>
         <p className="hidden sm:block hover:underline font-semibold">Deals</p>
         <p className="hidden lg:block hover:underline font-semibold">Grocery</p>
-        <p className="hidden md:block hover:underline font-semibold">
+        <p className="hidden lg:block hover:underline font-semibold">
           Essentials
         </p>
         <p className="hidden lg:block hover:underline font-semibold">Fashion</p>
-        <p className="hidden md:block hover:underline font-semibold">Ship</p>
+        <p className="hidden lg:block hover:underline font-semibold">Ship</p>
         <p className="hidden lg:block hover:underline font-semibold">
           Electronics
         </p>
