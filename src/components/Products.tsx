186608ca38236d0682e5ce9c368af14dda0d6d64
Zack Hanni <zack.hannI@gmail.com>
2024-03-28 23:51:57 -0400

    reduce name and description text

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,6 @@
 export const productCategory = [
   {
-    name: "Astrobiology and Terraforming Tools",
+    name: "Astrobiology and Terraforming",
     description:
       "Shape the future of humanity on distant worlds with OmniMart's astrobiology and terraforming tools. From microbial inoculants to portable greenhouse kits, we provide the resources needed to transform alien landscapes into habitable environments.",
     price: "$19.95",
@@ -21,16 +21,16 @@ export const productCategory = [
     image: "/pexels-kindel-media-8982664.jpg",
   },
   {
-    name: "Universal Communication Devices",
+    name: "Universal Communication",
     description:
       "Break down language barriers and stay connected across the stars with OmniMart's universal communication devices. Whether you're sending a message across the galaxy or communicating with extraterrestrial species, our secure and reliable devices have you covered.",
     price: "$19.95",
     image: "/pexels-petr-ganaj-17818760.jpg",
   },
   {
-    name: "Medical Supplies & Food Cube Rations",
+    name: "Medical Supplies",
     description:
-      "Ensure your health and well-being in the harsh conditions of space with OmniMart's advanced medical supplies. From portable diagnostic devices to emergency trauma kits, we offer everything you need to handle medical emergencies with confidence. ///Satisfy your hunger in the void of space with OmniMart's space-friendly food and nutrition options. From nutrient-packed meal replacements to hydroponic gardening kits, we provide innovative solutions to keep you nourished during your cosmic travels.",
+      "Ensure your health and well-being in the harsh conditions of space with OmniMart's advanced medical supplies. From portable diagnostic devices to emergency trauma kits, we offer everything you need to handle medical emergencies with confidence.",
     price: "$19.95",
     image: "/pexels-anna-shvets-6291281.jpg",
   },
