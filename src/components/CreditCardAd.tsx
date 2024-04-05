07f57cf7937915eab791c62cee39946d00706aa6
Zack Hanni <zack.hannI@gmail.com>
2024-04-05 16:59:59 -0400

    made ad section more responsive

--- src/components/CreditCardAd.tsx
+++ src/components/CreditCardAd.tsx
@@ -5,13 +5,15 @@ import React from "react";
 
 export default function CreditCardAd() {
   return (
-    <section className="container">
+    <section className="container bg-white">
       <div className="row">
-        <div className="text-center py-5 rounded-md shadow-xl outline outline-black/5 outline-1">
-          <FontAwesomeIcon icon={faCreditCard} className="text-2xl pr-4" />
-          <span>Earn 5% cash back.</span> See if you&apos;re pre-approved with
-          no credit risk.
-          <Link href="/" className="underline text-sm ml-4">
+        <div className=" flex justify-center flex-col sm:flex-row space-y-4 sm:space-y-0 text-center py-5 rounded-md shadow-xl outline outline-black/5 outline-1">
+          <FontAwesomeIcon icon={faCreditCard} className="text-2xl sm:pr-4" />
+          <div>
+            <span>Earn 5% cash back.</span> See if you&apos;re pre-approved with
+            no credit risk.
+          </div>
+          <Link href="/" className="underline text-sm sm:ml-4">
             Learn how
           </Link>
         </div>
