441e539a5ee35ae6248437a00c8c25cd7f48aeb2
Zack Hanni <zack.hannI@gmail.com>
2024-04-05 16:58:36 -0400

    removed pt, adjusted text size to be more responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="pt-[124px] relative sm:max-h-[625px] w-full overflow-hidden bg-black">
-      <div className="">
-        <div>
-          <Image
-            src="/galaxy.jpeg"
-            width={1920}
-            height={1080}
-            priority
-            alt="Hero image of lady holding bags"
-            className="w-full h-full"
-          />
-        </div>
-        <div className="absolute -translate-x-1/2 -translate-y-1/4 top-1/2 lg:top-1/2 left-1/2 text-white text-center w-full">
-          <p className="text-5xl font-bold">CARGO BAY SALE</p>
+    <section className="relative sm:max-h-[425px] w-full overflow-hidden">
+      <div className="h-[425px]">
+        <Image
+          src="/galaxy.jpeg"
+          width={1920}
+          height={1080}
+          priority
+          alt="Outer space galaxy"
+          className="w-full h-full object-cover"
+        />
 
-          <div className="flex items-center space-x-2 justify-center">
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-white text-center w-full">
+          <p className="display-5">CARGO BAY SALE</p>
+
+          <div className="text-xs sm:text-base flex items-center space-x-2 justify-center">
             <p>
               UP
               <br /> TO
             </p>
-            <p className="text-9xl">34% OFF</p>
+            <p className=" text-6xl sm:text-9xl">34% OFF</p>
           </div>
 
-          <p className="text-xl">
+          <p className="sm:text-xl">
             SAVE ON FLUX CAPACITORS, POWER CONVERTERS AND MORE!
           </p>
-          <button className="hover:bg-white outline outline-1 hover:outline-white hover:text-black rounded-full px-16 py-4 w-fit bg-black/20">
-            Shop now
-          </button>
+          <div className="gap-2 grid sm:flex justify-content-center sm:h-[72px]">
+            <button className="hover:bg-white outline outline-1 hover:outline-white hover:text-black rounded-full sm:px-16 px-8 sm:py-4 py-2 bg-black/20">
+              Shop now
+            </button>
+            {/* <Link href={"#flash_deals"} className=""> */}
+            <button className="hover:bg-white outline outline-1 outline-white hover:text-black rounded-full sm:px-16 px-8 sm:py-4 py-2 bg-black/20 ">
+              Flash deals
+            </button>
+            {/* </Link> */}
+          </div>
         </div>
       </div>
     </section>
