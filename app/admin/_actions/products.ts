57a212254be125986222715a56e3b4f1b0284df2
Zack Hanni <zack.hannI@gmail.com>
2024-06-28 17:03:51 -0400

    added updateProduct action and allow products to be edited

--- app/admin/_actions/products.ts
+++ app/admin/_actions/products.ts
@@ -51,6 +51,58 @@ export async function addProduct(prevState: unknown, formData: FormData) {
   redirect("/admin/products");
 }
 
+const editSchema = addSchema.extend({
+  file: fileSchema.optional(),
+  image: imageSchema.optional(),
+});
+
+export async function updateProduct(
+  id: string,
+  prevState: unknown,
+  formData: FormData
+) {
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+  const product = await db.product.findUnique({ where: { id } });
+
+  if (product == null) return notFound();
+
+  let filePath = product.filePath;
+  if (data.file != null && data.file.size > 0) {
+    await fs.unlink(product.filePath); // delete file
+    filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer())); // create new file path
+  }
+
+  let imagePath = product.imagePath;
+  if (data.image != null && data.image.size > 0) {
+    await fs.unlink(`public${product.imagePath}`); // delete image
+    imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+    await fs.writeFile(
+      // create new image path
+      `public${imagePath}`,
+      Buffer.from(await data.image.arrayBuffer())
+    );
+  }
+
+  await db.product.update({
+    where: { id },
+    data: {
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath,
+    },
+  });
+
+  redirect("/admin/products");
+}
+
 export async function toggleProductAvailability(
   id: string,
   isAvailableForPurchase: boolean
@@ -62,6 +114,7 @@ export async function deleteProduct(id: string) {
   const product = await db.product.delete({ where: { id } });
   if (product === null) return notFound();
 
+  // delete file and image when product is deleted
   await fs.unlink(product.filePath);
   await fs.unlink(`public${product.imagePath}`);
 }
