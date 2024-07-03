3cf9b9339b6f5724aa5de418e3d7562f41fa5280
Zack Hanni <zack.hannI@gmail.com>
2024-07-03 14:13:33 -0400

    rename nav items. added links. set icon sizes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -11,6 +11,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Logo from "./Logo";
+import Link from "next/link";
 
 export default function Nav() {
   const navMenuItems = [
@@ -23,19 +24,31 @@ export default function Nav() {
     <nav className="sticky top-0 w-full z-10 bg-gradient-to-r from-indigo-950 to-purple-700 text-white px-2">
       {/* top section nav */}
       <div className="flex flex-row justify-center items-center font-bold w-full px-4 space-x-4 ">
-        <div className="translate-y-1">
+        <Link href="/" className="translate-y-1">
           <Logo />
-        </div>
+        </Link>
 
-        <li className="hover:bg-purple-800 p-4 rounded-full hidden lg:block mb-0">
-          <FontAwesomeIcon icon={faRectangleList} className="pr-2 text-xl" />
+        <Link
+          href="/"
+          className="no-underline text-white hover:underline p-4 rounded-full hidden lg:block mb-0"
+        >
+          <FontAwesomeIcon
+            icon={faRectangleList}
+            className="pr-2 w-[22.5px] h-[17.5px]"
+          />
           Departments
-        </li>
+        </Link>
 
-        <li className="hover:bg-purple-800 p-4 rounded-full hidden xl:block mb-0">
-          <FontAwesomeIcon icon={faBellConcierge} className="pr-2 text-xl" />
-          Services
-        </li>
+        <Link
+          href="/"
+          className="no-underline text-white hover:underline p-4 rounded-full hidden xl:block mb-0"
+        >
+          <FontAwesomeIcon
+            icon={faBellConcierge}
+            className="pr-2 w-[20px] h-[15px]"
+          />
+          Products
+        </Link>
 
         <form className="min-w-[60%] lg:min-w-fit flex">
           <input
@@ -43,35 +56,41 @@ export default function Nav() {
             placeholder="Search OmniMart"
             name="nav-search"
           ></input>
-          <button className="pl-3 pr-1 hover:bg-purple-800 rounded-full text-center">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              className="pr-2 text-xl"
-            />
+          <button className="pl-3 pr-1 hover:underline rounded-full text-center">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="pr-2 size-5" />
           </button>
         </form>
 
-        <li className="hover:bg-purple-800 p-4 rounded-full hidden lg:block mb-0">
-          <FontAwesomeIcon icon={faHeart} className="pr-2 text-xl" />
+        <Link
+          href="/"
+          className="no-underline text-white hover:underline p-4 rounded-full hidden lg:block mb-0"
+        >
+          <FontAwesomeIcon
+            icon={faHeart}
+            className="pr-2 w-[20px] h-[17.08px]"
+          />
           My Items
-        </li>
+        </Link>
 
-        <div className="hover:bg-purple-800 p-4 rounded-full hidden lg:flex items-center">
-          <FontAwesomeIcon icon={faUser} className="pr-2 text-xl" />
+        <div className="no-underline text-white hover:underline p-4 rounded-full hidden lg:flex items-center">
+          <FontAwesomeIcon icon={faUser} className="pr-2 w-[17.5px] h-[20px]" />
           <div className="flex flex-col">
             <span className="font-normal text-sm -mb-1">Sign In</span>
             Account
           </div>
         </div>
 
-        <li className="flex flex-col">
-          <FontAwesomeIcon icon={faCartShopping} className="text-xl" />
+        <Link href="/" className="no-underline text-white flex flex-col">
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="w-[22.31px] h-[20px]"
+          />
           <span className="text-xs font-normal">$0.00</span>
-        </li>
+        </Link>
       </div>
       <hr className="my-0 hidden md:block" />
-      {/* bottom section nav */}
-      <div className="hidden md:flex items-center h-full space-x-6 pl-4 py-2 text-sm">
+      {/* bottom section - subnav */}
+      <ul className="hidden md:flex items-center h-full space-x-6 pl-4 py-2 text-s mb-0">
         <li className="list-none flex">
           <span className="hidden lg:block">Philadelphia, 19148 - </span>
           Milkyway Supercenter
@@ -98,7 +117,7 @@ export default function Nav() {
         <li className="hidden lg:block hover:underline font-semibold">
           Electronics
         </li>
-      </div>
+      </ul>
     </nav>
   );
 }
