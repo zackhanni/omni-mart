ba952652ed89ae1c6e96b3b7542e4f8b6d2530a4
Zack Hanni <zack.hannI@gmail.com>
2024-07-04 11:17:32 -0400

    removed old product cards

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -24,43 +24,6 @@ interface ProductCardProps {
   description?: string;
 }
 
-// export function ProductCardVertical(props: ProductCardProps) {
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   if (props.name.length > 45) {
-//     props.name = props.name.slice(0, 45 - props.name.length) + "...";
-//   }
-
-//   return (
-//     <div className="col relative">
-//       <div className="top-2 right-6 absolute text-xl">
-//         <FontAwesomeIcon
-//           icon={isFavorite ? faSolidHeart : faHeart}
-//           onClick={() => setIsFavorite(!isFavorite)}
-//         />
-//       </div>
-
-//       <div className="w-full h-[50%]">
-//         <Image
-//           src={props.image}
-//           width={300}
-//           height={500}
-//           alt="Picture of the product"
-//           className="w-full object-cover h-full"
-//         />
-//       </div>
-
-//       <div className="flex flex-col px-2 py-2">
-//         <p className="font-bold">{props.price}</p>
-//         <p className="">{props.name}</p>
-//         <button className="outline outline-1 hover:outline-2 rounded-full py-2 px-8 w-fit">
-//           + Add
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
 export function ProductCardVertical({
   id,
   name,
@@ -112,48 +75,6 @@ export function ProductCardVertical({
     </Card>
   );
 }
-
-//
-//
-//
-
-// export function ProductCardHorizontal(props: ProductCardProps) {
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   if (props.description && props.description.length > 60) {
-//     props.description =
-//       props.description.slice(0, 60 - props.description.length) + "...";
-//   }
-
-//   return (
-//     <div className="col-lg-6">
-//       <div className="row g-0">
-//         <div className="col bg-black/5 flex flex-col justify-center">
-//           <div className="px-8 ">
-//             <p className="font-semibold text-xl">{props.name}</p>
-//             <p className="hidden sm:block">{props.description}</p>
-//             <button className="outline outline-1 hover:outline-2 rounded-full py-2 px-6">
-//               See deals
-//             </button>
-//           </div>
-//         </div>
-//         <div className="col overflow-hidden sm:h-64">
-//           <Image
-//             src={props.image}
-//             width={300}
-//             height={500}
-//             alt="Picture of the product"
-//             className="object-fit"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-//
-//
-//
 
 type NewProductCardProps = {
   id: string;
