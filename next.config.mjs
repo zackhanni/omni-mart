d33e6077adf2c707dfd79bd0e885398ac0cdecb1
Zack Hanni <zack.hannI@gmail.com>
2024-09-11 16:45:24 -0400

    set up new db connection to supabase.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      os: false,
+      path: require.resolve("path-browserify"),
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
