37bec3f4d5fb5dc78c3d5d319d11d587656d2ffb
Zack Hanni <zack.hannI@gmail.com>
2024-09-11 12:27:43 -0400

    change hero centering. fix button link touchable size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className="relative sm:max-h-[425px] w-full overflow-hidden">
-      <div className="h-[425px]">
+      <div className="h-[425px] flex">
         <Image
           src="/galaxy.jpeg"
           width={1920}
@@ -15,7 +15,7 @@ export default function Hero() {
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-white text-center w-full">
+        <div className="absolute text-white text-center w-full h-full flex flex-col items-center justify-center bg-black/5">
           <p className="display-5">CARGO BAY SALE</p>
 
           <div className="text-xs sm:text-base flex items-center space-x-2 justify-center">
@@ -30,20 +30,20 @@ export default function Hero() {
             SAVE ON FLUX CAPACITORS, POWER CONVERTERS AND MORE!
           </p>
           <div className="gap-4 grid sm:flex justify-content-center sm:h-[72px]">
-            <button className="hover:bg-white outline outline-1 hover:outline-white hover:text-black rounded-full sm:px-16 px-8 sm:py-4 py-2 bg-black/30">
-              <Link
-                href="/products"
-                className="text-white hover:text-black no-underline"
-              >
+            <Link href="/products" className="no-underline text-white h-[72px]">
+              <button className="hover:bg-white outline outline-1 outline-white hover:text-black rounded-full h-16 w-52 bg-black/30 ">
                 All products
-              </Link>
-            </button>
+              </button>
+            </Link>
 
-            <button className="hover:bg-white outline outline-1 outline-white hover:text-black rounded-full sm:px-16 px-8 sm:py-4 py-2 bg-black/30 ">
-              <Link href={"#flash_deals"} className="no-underline text-white">
+            <Link
+              href={"#flash_deals"}
+              className="no-underline text-white h-[72px]"
+            >
+              <button className="hover:bg-white outline outline-1 outline-white hover:text-black rounded-full h-16 w-52 bg-black/30 ">
                 Flash deals
-              </Link>
-            </button>
+              </button>
+            </Link>
           </div>
         </div>
       </div>
